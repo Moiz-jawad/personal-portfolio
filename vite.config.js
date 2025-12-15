eff5e1dc0d0339e17f0be8be26f0f2b365eda59f
Moiz Jawad <moizxahito123@gmail.com>
2025-12-15 16:06:19 +0500

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // exposes network
+    port: 5173, // optional, default is 5173
+  },
+});
