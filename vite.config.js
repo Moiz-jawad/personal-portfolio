a9a6e7d978104c56e2ee3cc5fe0f3399ceecdf35
Moiz Jawad <moizxahito123@gmail.com>
2025-12-15 18:43:47 +0500

    changing some files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/personal-portfolio/",
+  base: "/",
   server: {
     host: true, // exposes dev server to local network
     port: 5173,
